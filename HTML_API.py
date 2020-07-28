a2db2510da649f444d527efe4d37b745045ce515
hideokawamoto <hideo.kawamoto@gmail.com>
2020-07-29 00:13:30 +0900

    Added logging Skipped initial scan of retrieved files

--- HTML_API.py
+++ HTML_API.py
@@ -1,37 +1,38 @@
 import random
 import time
 from requests_html import HTMLSession
+import requests
 
 
-def wait_time(min_wait=1, max_wait=2,*,logger):
+def wait_time(min_wait=1, max_wait=2, *, logger):
     min_wait = max(min_wait, 1)
     max_wait = max(max_wait, 2)
-    wait=max(random.normalvariate((min_wait + max_wait) / 2, abs(min_wait - max_wait)), 1)
-    logger.debug('wait time '+str(wait))
+    wait = max(random.normalvariate((min_wait + max_wait) / 2, abs(min_wait - max_wait)), 1)
+    logger.debug('wait time ' + str(wait))
     return wait
 
 
-def waited_get(session, url, min_wait=3, max_wait=5,*,logger):
+def waited_get(session: HTMLSession, url, min_wait=3, max_wait=5, *, logger):
     #    print(url)
-    logger.debug('get requested '+url)
-    time.sleep(wait_time(min_wait, max_wait,logger=logger))
+    logger.debug('get_requested ' + url)
+    time.sleep(wait_time(min_wait, max_wait, logger=logger))
     try:
-        res = session.get(url)
+        res = session.get(url + '/')
         res.raise_for_status()
-    except:
-        res=None
-    logger.debug('get processed '+url)
-    return res
+        logger.debug('get_processed ' + url)
+        return res
+    except requests.exceptions.ConnectionError as e:
+        logger.critical(url + " error " + e)
 
 
-def waited_post(session, url, data, min_wait=3, max_wait=5,*,logger):
+def waited_post(session: HTMLSession, url, data, min_wait=3, max_wait=5, *, logger):
     #    print(url)
-    logger.debug('post requested '+url)
-    time.sleep(wait_time(min_wait, max_wait,logger=logger))
+    logger.debug('post_requested ' + url)
+    time.sleep(wait_time(min_wait, max_wait, logger=logger))
     try:
         res = session.post(url, data)
         res.raise_for_status()
-    except:
-        res = None
-    logger.debug('post processed '+url)
-    return res
+        logger.debug('post_processed ' + url)
+        return res
+    except requests.exceptions.ConnectionError as e:
+        logger.critical(url + " error " + e)
--- PriceDataFrame.py
+++ PriceDataFrame.py
@@ -17,7 +17,7 @@ def load_price_df(base_path):
         for k in header.keys():
             df1[k] = header[k]
         stockprice_df = pd.concat([stockprice_df, df1])
-        print('loaded ' + str(len(stockprice_df)) + ' records\r',end='')
+        print('loaded ' + str(len(stockprice_df)) + ' records\r', end='')
     stockprice_df['date'] = pd.to_datetime(stockprice_df['date'])
     return stockprice_df
 
@@ -25,12 +25,12 @@ def load_price_df(base_path):
 def retrieved_df(base_path):
     base_folder = path.Path(base_path).expanduser().resolve()
     df = pd.DataFrame(columns=['code', 'year'])
-    i=0
+    i = 0
     for f in sorted(base_folder.resolve(strict=True).glob('*.csv')):
-        fname_elements = re.split('[_\.]', f.name)
+        fname_elements = re.split('[_.]', f.name)
         df = df.append(pd.Series([fname_elements[0], fname_elements[1]], index=['code', 'year']), ignore_index=True)
-        i+=1
-        print('['+str(i)+']: '+f.name + ' processed.\r',end='')
+        i += 1
+        print('[' + str(i) + ']: ' + f.name + ' processed.\r', end='')
     df.drop_duplicates(inplace=True)
     df.to_csv(path.Path.joinpath(base_folder, '.retrieved.csv'))
     return df
