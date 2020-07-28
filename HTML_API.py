a8d96e454260a842d5e0560e83a95e57ef6115bf
hideokawamoto <hideo.kawamoto@gmail.com>
2020-07-28 23:49:47 +0900

    Added logging Skipped initial scan of retrieved files

--- HTML_API.py
+++ HTML_API.py
@@ -1,30 +1,37 @@
 import random
 import time
+from requests_html import HTMLSession
 
 
-def wait_time(min_wait=1, max_wait=2):
+def wait_time(min_wait=1, max_wait=2,*,logger):
     min_wait = max(min_wait, 1)
     max_wait = max(max_wait, 2)
-    return max(random.normalvariate((min_wait + max_wait) / 2, abs(min_wait - max_wait)) / 4, min_wait)
+    wait=max(random.normalvariate((min_wait + max_wait) / 2, abs(min_wait - max_wait)), 1)
+    logger.debug('wait time '+str(wait))
+    return wait
 
 
-def waited_get(session, url, min_wait=3, max_wait=5):
+def waited_get(session, url, min_wait=3, max_wait=5,*,logger):
     #    print(url)
-    time.sleep(wait_time(min_wait, max_wait))
+    logger.debug('get requested '+url)
+    time.sleep(wait_time(min_wait, max_wait,logger=logger))
     try:
         res = session.get(url)
         res.raise_for_status()
     except:
         res=None
+    logger.debug('get processed '+url)
     return res
 
 
-def waited_post(session, url, data, min_wait=3, max_wait=5):
+def waited_post(session, url, data, min_wait=3, max_wait=5,*,logger):
     #    print(url)
-    time.sleep(wait_time(min_wait, max_wait))
+    logger.debug('post requested '+url)
+    time.sleep(wait_time(min_wait, max_wait,logger=logger))
     try:
         res = session.post(url, data)
         res.raise_for_status()
     except:
         res = None
+    logger.debug('post processed '+url)
     return res
--- StockPrice_HTML.py
+++ StockPrice_HTML.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
-
 
 import pathlib as path
 import re
@@ -15,55 +13,73 @@ import Declaration
 import PriceDataFrame
 from Declaration import filePath
 from HTML_API import waited_get
+from logging import getLogger,basicConfig,DEBUG,Formatter
+
+LogFormat='{asctime} [{levelname}] {module} {message}'
+basicConfig(filename='stockprice_retrieval.log', filemode='w', format=LogFormat,style='{',level=DEBUG)
+logger=getLogger(__name__)
+
+logger.info('StockPrice retrieval started')
 
 # stockprice_df = PriceDataFrame.load_price_df(filePath)
-retrieved_df = PriceDataFrame.retrieved_df(filePath)
+#retrieved_df = PriceDataFrame.retrieved_df(filePath)
 
 # Start session
 
 session = HTMLSession()
-res = waited_get(session, Declaration.url)
+res = waited_get(session, Declaration.url,logger=logger)
 
 # stock list
-stock_df = pd.DataFrame(columns=Declaration.stock_list_header)
 # In[36]:
 
 # print(stock_df)
 for page_link in sorted([link for link in res.html.absolute_links if re.search('page=', link)]):
-    res = waited_get(session, page_link)
+    logger.debug('started '+page_link)
+    stock_df = pd.DataFrame(columns=Declaration.stock_list_header)
+    res = waited_get(session, page_link,logger=logger)
+    if len(pd.read_html(res.text))==0:
+        logger.warning(page_link + ' has_nothing')
+        continue
     df = pd.read_html(res.text)[0][['コード・名称', '市場']]
     df = pd.concat([df['コード・名称'].str.split(' ', 1, expand=True), df['市場']], axis=1)
     df.columns = Declaration.stock_list_header
     stock_df = stock_df.append(df)
-    print(str(len(stock_df)) + ' issue names are captured.\r',end='')
+    logger.debug('processed ' + page_link)
 
-for code in stock_df['code']:
-    print(urljoin(Declaration.url, str(code)))
-    res = waited_get(session, urljoin(Declaration.url, str(code) + '/'))
-    year_links = sorted([link for link in res.html.absolute_links if re.search("/stock/[01-9]+/[01-9]+", link)])
-    for year_link in year_links:
-        print(year_link)
-        year = year_link.split('/')[-2]
-        code = year_link.split('/')[-3]
-        if ((retrieved_df['year'] == year) & (retrieved_df['code'] == code)).sum() > 0:
-            print('skipped')
-            continue
-        else:
-            res = waited_get(session, year_link)
-            if res==None: continue
-            df = pd.read_html(res.text)[0]
-            df.columns = Declaration.field_names
-            df['code'] = code
-            df['issue'] = stock_df[(stock_df['code'] == code)]['issue'].values[0]
-            df['market'] = stock_df[(stock_df['code'] == code)]['market'].values[0]
-            df['date'] = pd.to_datetime(df['date'])
-            df.to_csv(path.Path(filePath).expanduser().resolve().joinpath(str(code) + '_' + str(year) + '.csv'))
+    for code in stock_df['code']:
+        base_folder = path.Path(filePath).expanduser().resolve()
+        #print(urljoin(Declaration.url, str(code)))
+        res = waited_get(session, urljoin(Declaration.url, str(code) + '/'),logger=logger)
+        year_links = sorted([link for link in res.html.absolute_links if re.search("/stock/[01-9]+/[01-9]+", link)])
+        for year_link in year_links:
+            #print(year_link)
+            year = year_link.split('/')[-2]
+            code = year_link.split('/')[-3]
+            if base_folder.joinpath(str(code) + '_' + str(year) + '.csv').is_file():
+                logger.debug('skipped ' + year_link)
+                #print('skipped')
+            else:
+                logger.debug('started ' + year_link)
+                res = waited_get(session, year_link,logger=logger)
+                if len(pd.read_html(res.text))==0:
+                    logger.warning(year_link + ' has_nothing')
+                    continue
+                df = pd.read_html(res.text)[0]
+                df.columns = Declaration.field_names
+                df['code'] = code
+                df['issue'] = stock_df[(stock_df['code'] == code)]['issue'].values[0]
+                df['market'] = stock_df[(stock_df['code'] == code)]['market'].values[0]
+                df['date'] = pd.to_datetime(df['date'])
+                fileName=path.Path(filePath).expanduser().resolve().joinpath(str(code) + '_' + str(year) + '.csv')
+                df.to_csv(fileName)
+                print ('added '+fileName)
+                logger.info ('added '+fileName)
+                logger.debug('processed ' + year_link)
             # stockprice_df = stockprice_df.append(df)
 
 # In[39]:
 
 
-print(stock_df)
 
 # In[23]:
 
