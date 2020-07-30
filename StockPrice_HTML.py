1b794f07762c8b52031f992c938ceb1e99fb87f9
hideokawamoto <hideo.kawamoto@gmail.com>
2020-07-31 00:29:18 +0900

    Added logging Skipped initial scan of retrieved files

--- StockPrice_HTML.py
+++ StockPrice_HTML.py
@@ -4,7 +4,7 @@
 
 import pathlib as path
 import re
-from logging import getLogger, basicConfig, DEBUG
+from logging import getLogger, basicConfig, DEBUG, INFO, Formatter
 from urllib.parse import urljoin
 
 import pandas as pd
@@ -15,8 +15,7 @@ from Declaration import filePath
 from HTML_API import waited_get
 
 LogFormat = '{asctime} [{levelname}] {module} {message}'
-# Formatter(LogFormat, style='{')
-basicConfig(format=LogFormat, style='{', level=DEBUG)
+basicConfig(filename='stockprice.log',format=LogFormat, style='{', level=INFO)
 logger = getLogger(__name__)
 
 logger.info('StockPrice retrieval started')
@@ -28,17 +27,28 @@ logger.info('StockPrice retrieval started')
 
 session = HTMLSession()
 res = waited_get(session, Declaration.url, logger=logger)
+page_links = []
+try:
+    page_links = [link for link in res.html.absolute_links if re.search('page=', link)]
+except ValueError:
+    logger.critical('invalid_url ' + Declaration.url)
+    raise
 
 # stock list
 # In[36]:
 
 # print(stock_df)
-for page_link in ([link for link in res.html.absolute_links if re.search('page=', link)]):
+for page_link in page_links:
     logger.debug('started ' + page_link)
     stock_df = pd.DataFrame(columns=Declaration.stock_list_header)
     res = waited_get(session, page_link, logger=logger)
     try:
         df = pd.read_html(res.text)[0][['コード・名称', '市場']]
+    except ValueError as e:
+        logger.warning(page_link + ' has_nothing')
+    except AttributeError as e:
+        logger.warning(page_link + ' AttributeError')
+    else:
         df = pd.concat([df['コード・名称'].str.split(' ', 1, expand=True), df['市場']], axis=1)
         df.columns = Declaration.stock_list_header
         stock_df = stock_df.append(df)
@@ -47,37 +57,43 @@ for page_link in ([link for link in res.html.absolute_links if re.search('page='
         for code in stock_df['code']:
             base_folder = path.Path(filePath).expanduser().resolve()
             # print(urljoin(Declaration.url, str(code)))
-            res = waited_get(session, urljoin(Declaration.url, str(code) + '/'), logger=logger)
-            year_links = sorted([link for link in res.html.absolute_links if re.search("/stock/[01-9]+/[01-9]+", link)])
-            for year_link in year_links:
-                # print(year_link)
-                year = year_link.split('/')[-2]
-                code = year_link.split('/')[-3]
-                if base_folder.joinpath(str(code) + '_' + str(year) + '.csv').is_file():
-                    logger.info('skipped ' + year_link)
-                    # print('skipped')
-                else:
-                    logger.debug('started ' + year_link)
-                    res = waited_get(session, year_link, logger=logger)
-                    try:
-                        df = pd.read_html(res.text)[0]
-                        df.columns = Declaration.field_names
-                        df['code'] = code
-                        df['issue'] = stock_df[(stock_df['code'] == code)]['issue'].values[0]
-                        df['market'] = stock_df[(stock_df['code'] == code)]['market'].values[0]
-                        df['date'] = pd.to_datetime(df['date'])
-                        fileName = path.Path(filePath).expanduser().resolve()
-                        fileName = fileName.joinpath(str(code) + '_' + str(year) + '.csv')
-                        df.to_csv(fileName)
-                        print('added ' + fileName.name)
-                        logger.info('added ' + fileName.name)
-                        logger.debug('processed ' + year_link)
-                    except ValueError:
-                        logger.warning(year_link + ' has_nothing')
-                        continue
-    except ValueError:
-        logger.warning(page_link + ' has_nothing')
-        continue
+            code_link = urljoin(Declaration.url, str(code) + '/')
+            res = waited_get(session, code_link, logger=logger)
+            try:
+                year_links = sorted([link for link in res.html.absolute_links if re.search("/stock/[01-9]+/[01-9]+", link)])
+            except ValueError as e:
+                logger.warning(code_link + ' ValueError')
+            except AttributeError as e:
+                logger.warning(code_link + ' AttributeError')
+            else:
+                for year_link in year_links:
+                    # print(year_link)co
+                    year = year_link.split('/')[-2]
+                    code = year_link.split('/')[-3]
+                    if base_folder.joinpath(str(code) + '_' + str(year) + '.csv').is_file():
+                        logger.info('skipped ' + year_link)
+                        # print('skipped')
+                    else:
+                        logger.debug('started ' + year_link)
+                        res = waited_get(session, year_link, logger=logger)
+                        try:
+                            df = pd.read_html(res.text)[0]
+                        except ValueError as e:
+                            logger.warning(year_link + ' ValueError')
+                        except AttributeError as e:
+                            logger.warning(year_link + ' AttributeError')
+                        else:
+                            df.columns = Declaration.field_names
+                            df['code'] = code
+                            df['issue'] = stock_df[(stock_df['code'] == code)]['issue'].values[0]
+                            df['market'] = stock_df[(stock_df['code'] == code)]['market'].values[0]
+                            df['date'] = pd.to_datetime(df['date'])
+                            fileName = path.Path(filePath).expanduser().resolve()
+                            fileName = fileName.joinpath(str(code) + '_' + str(year) + '.csv')
+                            df.to_csv(fileName)
+                            print('added ' + fileName.name)
+                            logger.info('added ' + fileName.name)
+                            logger.debug('processed ' + year_link)
 
 # In[39]:
 
