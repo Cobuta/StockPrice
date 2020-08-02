d87d9fa2b901857d01e516b40405f6c877972c5c
hideokawamoto <hideo.kawamoto@gmail.com>
2020-08-02 16:14:09 +0900

    Added Commandline Parameters

--- Declaration.py
+++ Declaration.py
@@ -2,5 +2,3 @@ header_keys = ('code', 'market', 'issue')
 field_names = ('date', 'open', 'high', 'low', 'close', 'volume', 'adj_close')
 field_map = {'日付':'date', '始値':'open','高値': 'high', '安値':'low','終値' :'close','出来高': 'volume', '終値調整':'adj_close'}
 stock_list_header = ('code', 'issue', 'market')
-url = 'https://kabuoji3.com/stock/'
-filePath = '~/Dropbox/StockPrice/'
